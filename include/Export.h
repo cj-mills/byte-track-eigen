009d4e7ccd1517fa4d653494d132509c909a1920
Christian J. Mills <millscj+github@protonmail.com>
2023-12-16 13:32:06 -0800

    Update Export.h

--- include/Export.h
+++ include/Export.h
@@ -1,5 +1,13 @@
 #ifdef BUILDING_BYTE_TRACK_EIGEN
+#ifdef _WIN32
 #define BYTE_TRACK_EIGEN_API __declspec(dllexport)
 #else
+#define BYTE_TRACK_EIGEN_API __attribute__((visibility("default")))
+#endif
+#else
+#ifdef _WIN32
 #define BYTE_TRACK_EIGEN_API __declspec(dllimport)
+#else
+#define BYTE_TRACK_EIGEN_API
+#endif
 #endif
